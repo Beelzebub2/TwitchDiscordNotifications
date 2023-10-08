44a3f1a7a1e2817be0e6cd982f3c22ef65c68d2a
Beelzebub2 <ricardorocamartins2004@gmail.com>
2023-10-08 13:31:12 +0100

    v2.2

--- functions/updater.py
+++ functions/updater.py
@@ -31,26 +31,45 @@ def set_console_title(title):
 online_version = "v" + \
     functions.others.get_version(
         "https://github.com/Beelzebub2/TwitchDiscordNotifications")
-print(online_version)
-print(current_version)
 
 
-def search_for_updates():
+def search_for_updates(autoupdate=False):
     try:
+        print(online_version)
+        print(current_version)
         set_console_title("Checking For Updates. . .")
-        print(" " * console_width, end="\r")
-        functions.others.log_print(
-            f"{Fore.LIGHTYELLOW_EX}[INFO] Checking for updates{Fore.RESET}")
+        print(" " * console_width, end="\r\r")
 
         if online_version != current_version:
             set_console_title("New Update Found!")
-            choice = input(
-                f"{Fore.GREEN}[{Fore.YELLOW}>>>{Fore.GREEN}] {Fore.RESET}You want to update to the latest version? (Y to update): {Fore.RED}"
-            )
+            if not autoupdate:
+                choice = input(
+                    f"{Fore.GREEN}[{Fore.YELLOW}>>>{Fore.GREEN}] {Fore.RESET}You want to update to the latest version? (Y to update): {Fore.RED}"
+                )
 
-            if choice.lower() == "y" or choice.lower() == "yes":
-                print(f"{Fore.WHITE}\nUpdating. . .")
-                set_console_title("Updating...")
+                if choice.lower() == "y" or choice.lower() == "yes":
+                    print(f"{Fore.WHITE}\nUpdating. . .")
+                    set_console_title("Updating...")
+                    new = requests.get(
+                        f"https://github.com/Beelzebub2/TwitchDiscordNotifications/archive/refs/heads/main.zip"
+                    )
+                    with open("TwitchDiscordNotifications.zip", "wb") as zipfile:
+                        zipfile.write(new.content)
+                    with ZipFile("TwitchDiscordNotifications.zip", "r") as filezip:
+                        filezip.extractall()
+                    os.remove("TwitchDiscordNotifications.zip")
+                    cwd = os.getcwd() + "\\TwitchDiscordNotifications-main"
+                    shutil.copytree(cwd, os.getcwd(), dirs_exist_ok=True)
+                    shutil.rmtree(cwd)
+                    ch.set_version(online_version)
+                    clear()
+                    set_console_title(f"Update Successfully Finished!")
+                    functions.others.log_print(
+                        f"{Fore.LIGHTGREEN_EX}[SUCCESS] {Fore.LIGHTWHITE_EX}Updated bot from version {Fore.LIGHTYELLOW_EX + current_version + Fore.RESET} to { Fore.LIGHTGREEN_EX + online_version}")
+                    print("Attempting to start bot...")
+                    time.sleep(2)
+                    return True
+            if autoupdate:
                 new = requests.get(
                     f"https://github.com/Beelzebub2/TwitchDiscordNotifications/archive/refs/heads/main.zip"
                 )
